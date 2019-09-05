243c1397e0b17e7777ab6e5139c84c12fa84dd4f
a-jumani <arsalan.jumani@outlook.com>
2019-09-05 18:04:33 +0400

    feat: Support MoveInsertable objects in stacks_min

--- stacks/stack_mins.hpp
+++ stacks/stack_mins.hpp
@@ -2,19 +2,22 @@
 #include <stack>
 #include <vector>
 
-// TODO: remove copying overhead
-
 /**
  * Stack data structure like std::stack but additionally
- * supports get_min().
- * 
- * Note: may involve copying of Comparable objects.
+ * supports get_min(). It also supports MoveInsertable objects.
  */
 template <typename Comparable, typename Compare = std::less<Comparable> >
 class stack_mins {
 private:
     std::vector<Comparable> main_stack;     // actual values
     std::stack<std::size_t> mins_stack;     // index of mins
+    
+    // update current min, if needed, for to-be inserted Comparable object
+    void update_min(Comparable &x) {
+        if ( mins_stack.empty() || comp(x, main_stack[mins_stack.top()]) )
+            mins_stack.push(main_stack.size());
+    }
+
 public:
     
     Compare comp;
@@ -24,9 +27,17 @@ public:
        Add an element to the stack.
      */
     void push(const Comparable &x) {
-        if ( mins_stack.empty() || comp(x, main_stack[mins_stack.top()]) )
-            mins_stack.push(main_stack.size());
+        update_min(x);
         main_stack.push_back(x);
+    }
+
+    /**
+     * Add an element to the stack. Supports MoveInsertable
+     * elements.
+     */
+    void push(Comparable &&x) {
+        update_min(x);
+        main_stack.push_back(std::move(x));
     }
 
     /**
@@ -36,6 +47,15 @@ public:
         if ( main_stack.size() - 1 == mins_stack.top() )
             mins_stack.pop();
         main_stack.pop_back();
+    }
+
+    /**
+     * Remove element from top of stack. Useful for
+     * MoveInsertable elements.
+     */
+    void pop(Comparable &x) {
+        x = std::move(main_stack.back());
+        pop();
     }
 
     /**
